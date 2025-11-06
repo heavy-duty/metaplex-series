92aa68113c003deb7bff6caf9b722fd741f2fa08
Dan <danielarturomt@gmail.com>
2025-11-06 14:39:40 +0000

    chore: remove episode structure

--- episodes/episode-6/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./calculate-payment-orders";
-export * from "./create-command";
-export * from "./fetch-asset-with-metadata";
-export * from "./get-umi";
-export * from "./read-keypair-from-file";
-export * from "./to-campaign";
-export * from "./upload-image";
-
