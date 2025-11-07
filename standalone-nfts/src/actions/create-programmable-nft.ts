dfc128a44c0bd95d2f0b5091ca6f19604dd40a1a
Dan <danielarturomt@gmail.com>
2025-11-07 11:28:20 +0000

    chore: remove code from activities

--- standalone-nfts/src/actions/create-programmable-nft.ts
+++ standalone-nfts/src/actions/create-programmable-nft.ts
@@ -1,43 +1,8 @@
-import { createProgrammableNft } from "@metaplex-foundation/mpl-token-metadata";
-import {
-  createGenericFile,
-  generateSigner,
-  percentAmount,
-  Umi,
-} from "@metaplex-foundation/umi";
-import { base58 } from "@metaplex-foundation/umi/serializers";
-import { readFile } from "fs/promises";
-import path from "path";
+import { Umi } from "@metaplex-foundation/umi";
 
-export async function createProgrammableNftAction(umi: Umi) {
+export async function createProgrammableNftAction(_umi: Umi) {
   // Subimos la imagen a la red de irys
-  const imagePath = path.join(__dirname, "../../assets/image.png");
-  const imageBuffer = await readFile(imagePath);
-  const imageFile = createGenericFile(imageBuffer, imagePath, {
-    contentType: "image/png",
-  });
-  const [image] = await umi.uploader.upload([imageFile]);
-
   // Subimos la metadata a la red de irys
-  const uri = await umi.uploader.uploadJson({
-    name: "Mi primer NFT programable",
-    symbol: "FAM",
-    description: "Este es mi primer NFT programable de Metaplex",
-    image,
-  });
-
   // Generamos el signer que sera asociado al mint
-  const mintSigner = generateSigner(umi);
-  console.log(`   > Mint address: ${mintSigner.publicKey}`);
-
   // Minteamos el NFT en la red de solana
-  const createSignature = await createProgrammableNft(umi, {
-    mint: mintSigner,
-    name: "Mi NFT",
-    uri,
-    sellerFeeBasisPoints: percentAmount(0),
-  }).sendAndConfirm(umi);
-  console.log(
-    `   > Create signature: ${base58.deserialize(createSignature.signature)[0]}`,
-  );
 }
