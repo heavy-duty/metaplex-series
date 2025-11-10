7ffcd15ad8f25de5e98da496a7031de611493700
Dan <danielarturomt@gmail.com>
2025-11-10 13:06:51 +0000

    chore: remove code snippets

--- crowdfunding/src/commands/create-campaign.command.ts
+++ crowdfunding/src/commands/create-campaign.command.ts
@@ -1,9 +1,3 @@
-import { create as createCoreNft } from "@metaplex-foundation/mpl-core";
-import { generateSigner } from "@metaplex-foundation/umi";
-import { base58 } from "@metaplex-foundation/umi/serializers";
-import path from "path";
-import { getUmi, readKeypairFromFile, uploadImage } from "../utils";
-
 export interface CreateCampaignCommandOptions {
   goal: string;
   name: string;
@@ -19,51 +13,9 @@ export async function createCampaignCommand(
   options: CreateCampaignCommandOptions,
 ) {
   // Inicializamos Umi
-  const umi = await getUmi(options.serverKeypair);
-
   // Leemos el keypair del creador
-  const creatorKeypair = await readKeypairFromFile(umi, options.creatorKeypair);
-
   // Subimos la imagen de la campaña
-  const campaignImage = await uploadImage(
-    umi,
-    path.join(__dirname, "../../assets", "campaign-image.png"),
-  );
-
   // Subimos la metadata de la campaña
-  const campaignUri = await umi.uploader.uploadJson({
-    name: options.name,
-    symbol: options.symbol,
-    description: options.description,
-    image: campaignImage,
-    attributes: [
-      { trait_type: "goal", value: options.goal },
-      { trait_type: "creatorWallet", value: creatorKeypair.publicKey },
-    ],
-  });
-
   // Generamos el signer asociado al NFT de la campaña
-  const campaignSigner = generateSigner(umi);
-
   // Creamos el NFT de la campaña usando core
-  const createCampaignSignature = await createCoreNft(umi, {
-    asset: campaignSigner,
-    name: options.name,
-    uri: campaignUri,
-    plugins: [
-      {
-        type: "Attributes",
-        attributeList: [
-          { key: "status", value: "draft" },
-          { key: "totalPledges", value: "0" },
-          { key: "refundedPledges", value: "0" },
-        ],
-      },
-    ],
-  }).sendAndConfirm(umi);
-  console.log(
-    `Create Campaign NFT (address: ${campaignSigner.publicKey}) signature: ${
-      base58.deserialize(createCampaignSignature.signature)[0]
-    }`,
-  );
 }
--- crowdfunding/src/commands/refund-campaign.command.ts
+++ crowdfunding/src/commands/refund-campaign.command.ts
@@ -1,26 +1,3 @@
-import {
-  burn,
-  execute,
-  fetchAssetV1,
-  fetchCollection,
-  findAssetSignerPda,
-  updatePlugin,
-} from "@metaplex-foundation/mpl-core";
-import { transferSol } from "@metaplex-foundation/mpl-toolbox";
-import {
-  createNoopSigner,
-  createSignerFromKeypair,
-  publicKey,
-  sol,
-} from "@metaplex-foundation/umi";
-import { base58 } from "@metaplex-foundation/umi/serializers";
-import {
-  fetchAssetWithMetadata,
-  getUmi,
-  readKeypairFromFile,
-  toCampaign,
-} from "../utils";
-
 export interface RefundCampaignCommandOptions {
   campaignAssetAddress: string;
   pledgeAssetAddress: string;
@@ -34,101 +11,16 @@ export async function refundCampaignCommand(
   options: RefundCampaignCommandOptions,
 ) {
   // Inicializamos Umi
-  const umi = await getUmi(options.serverKeypair);
-
   // Leemos el keypair del backer
-  const backerKeypair = await readKeypairFromFile(umi, options.backerKeypair);
-
   // Obtenemos el NFT de la campaña con su metadata
-  const campaignAssetWithMetadata = await fetchAssetWithMetadata({
-    serverKeypair: options.serverKeypair,
-    campaignAssetAddress: options.campaignAssetAddress,
-  });
-
   // Transformamos el NFT de la campaña en un objeto de tipo campaña
-  const campaign = toCampaign(campaignAssetWithMetadata);
-
   // Validamos que la campaña este activa
-  if (campaign.status !== "active") {
-    throw new Error("Only active campaigns can have refunds");
-  }
-
   // Obtenemos el NFT del pledge
-  const pledgeAsset = await fetchAssetV1(
-    umi,
-    publicKey(options.pledgeAssetAddress),
-  );
-
   // Validamos que el owner del NFT del pledge coincide con el keypair del backer dado
-  if (pledgeAsset.owner !== backerKeypair.publicKey) {
-    throw new Error("You are not authorized to refund with this pledge");
-  }
-
   // Obtenemos la coleccion de pledges
-  const pledgesCollection = await fetchCollection(
-    umi,
-    publicKey(campaign.pledgesCollectionAddress),
-  );
-
   // Quemamos el NFT del pledge
-  const burnPledgeSignature = await burn(umi, {
-    asset: pledgeAsset,
-    collection: pledgesCollection,
-    authority: createSignerFromKeypair(umi, backerKeypair),
-  }).sendAndConfirm(umi);
-  console.log(
-    `Burn Pledge (address: ${options.pledgeAssetAddress}) signature: ${
-      base58.deserialize(burnPledgeSignature.signature)[0]
-    }`,
-  );
-
   // Buscamos la direccion del asset signer de la campaña
-  const campaignAssetSignerPda = findAssetSignerPda(umi, {
-    asset: publicKey(campaign.address),
-  });
-
   // Creamos un signer que no firme para el asset signer
-  const campaignAssetSigner = createNoopSigner(campaignAssetSignerPda[0]);
-
   // Transferimos del asset signer al backer
-  const transferSolSignature = await execute(umi, {
-    asset: campaignAssetWithMetadata,
-    instructions: transferSol(umi, {
-      amount: sol(0.001),
-      source: campaignAssetSigner,
-      destination: backerKeypair.publicKey,
-    }).getInstructions(),
-    payer: umi.identity,
-    assetSigner: campaignAssetSignerPda,
-  }).sendAndConfirm(umi);
-  console.log(
-    `Transfer 0.001 SOL (to address: ${backerKeypair.publicKey}) signature: ${
-      base58.deserialize(transferSolSignature.signature)[0]
-    }`,
-  );
-
   // Actualizamos los pledges reembolsados en la campaña
-  const updateCampaignSignature = await updatePlugin(umi, {
-    asset: publicKey(options.campaignAssetAddress),
-    plugin: {
-      type: "Attributes",
-      attributeList: [
-        { key: "status", value: "active" },
-        {
-          key: "pledgesCollectionAddress",
-          value: publicKey(campaign.pledgesCollectionAddress),
-        },
-        { key: "totalPledges", value: campaign.totalPledges.toString() },
-        {
-          key: "refundedPledges",
-          value: (campaign.refundedPledges + 1).toString(),
-        },
-      ],
-    },
-  }).sendAndConfirm(umi);
-  console.log(
-    `Update Campaign (address: ${campaign.address}) signature: ${
-      base58.deserialize(updateCampaignSignature.signature)[0]
-    }`,
-  );
 }
