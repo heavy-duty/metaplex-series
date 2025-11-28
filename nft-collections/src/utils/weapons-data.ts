b23aadef024e781b3bd549ae51935582d82540bb
Dan <danielarturomt@gmail.com>
2025-11-28 13:17:57 +0000

    chore: add the weapons-data

---
+++ nft-collections/src/utils/weapons-data.ts
@@ -0,0 +1,50 @@
+export const weaponsData = [
+  {
+    name: "Thunderplate",
+    description: "Rune-etched armor pulsing with storm energy",
+    imageName: "item_1",
+    attributes: [
+      { trait_type: "Material", value: "Uru" },
+      { trait_type: "Nivel", value: "30" },
+    ],
+  },
+  {
+    name: "Mjolnir",
+    description:
+      "Thunder god's hammer forged in starfire, blessed by Odin himself.",
+    imageName: "item_2",
+    attributes: [
+      { trait_type: "Material", value: "Uru" },
+      { trait_type: "Nivel", value: "50" },
+      { trait_type: "Enchantment", value: "Odin's Blessing" },
+      { trait_type: "Rarity", value: "Divine" },
+    ],
+  },
+  {
+    name: "Valkyrie Guard",
+    description: "Winged shield forged in Asgardian runes",
+    imageName: "item_3",
+    attributes: [
+      { trait_type: "Material", value: "Uru" },
+      { trait_type: "Nivel", value: "30" },
+    ],
+  },
+  {
+    name: "Stormhelm",
+    description: "Horned helm with ethereal wings and lightning",
+    imageName: "item_4",
+    attributes: [
+      { trait_type: "Material", value: "Uru" },
+      { trait_type: "Nivel", value: "30" },
+    ],
+  },
+  {
+    name: "Stormcleaver",
+    description: "Lightning-charged axe of Norse design",
+    imageName: "item_5",
+    attributes: [
+      { trait_type: "Material", value: "Uru" },
+      { trait_type: "Nivel", value: "30" },
+    ],
+  },
+];
